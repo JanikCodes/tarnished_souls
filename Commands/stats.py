f5442fdb17a5fc20c6f8aaa28fbdbeb224d4e1db
JanikCodes <piefke2000@gmx.de>
2023-03-25 00:08:11 +0100

    Added first test cog command

---
+++ Commands/stats.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+class stats(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Bot is Online. Beep bop.")
+
+    @commands.command()
+    async def stats(selfself, ctx):
+        await ctx.send("Hey!")
+
+async def setup(bot):
+    await bot.add_cog(stats(bot))
--- TarnishedBot.py
+++ TarnishedBot.py
@@ -1,3 +1,5 @@
+import os
+import asyncio
 import discord
 from discord.ext import commands
 import json
@@ -10,9 +12,15 @@ with open('bot.json') as file:
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
-result = database.Get_Users()
-print(result)
 
-bot.run(botConfig["token"])
+async def load():
+    for fileName in os.listdir('./Commands'):
+        if fileName.endswith('.py'):
+            await bot.load_extension(f'Commands.{fileName[:-3]}')
 
+async def main():
+    await load()
+    await bot.start(botConfig["token"])
+
+asyncio.run(main())
 
