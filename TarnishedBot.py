021db7faed9e3b0b813ffa1229f56d8d46d63ce1
JanikCodes <piefke2000@gmx.de>
2023-03-24 23:55:54 +0100

    Added database connection

--- TarnishedBot.py
+++ TarnishedBot.py
@@ -1,23 +1,18 @@
 import discord
+from discord.ext import commands
 import json
-
-class TarnishedBot(discord.Client):
-    async def on_ready(self):
-        print("I'm logged in. Beep bop.")
-
-    async def on_message(self, message):
-        if message.author == self.user:
-            return
-        
-        if message.content.startwith("!stats"):
-            completeText = message.content.split(" ")
-
+import mysql.connector
+import database
 
 #load bot token from json
 with open('bot.json') as file:
     botConfig = json.load(file)
 
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+result = database.Get_Users()
+print(result)
 
-intents = discord.Intents.default()
-bot = TarnishedBot(intents=intents)
 bot.run(botConfig["token"])
+
+
--- database.py
+++ database.py
@@ -1,11 +1,27 @@
+import discord
+from discord.ext import commands
+import json
 import mysql.connector
 
+# Connect to the database
 mydb = mysql.connector.connect(
     host='localhost',
     user='root',
     password='1111',
-    port='3386',
+    port='3306',
     database='tarnished'
 )
 
-mysql.connector.connect()
+cursor = mydb.cursor()
+
+# Check if the connection is alive
+if mydb.is_connected():
+  print("Database connection successful")
+else:
+  print("Database connection failed")
+
+# Define a function that accesses the cursor
+def Get_Users():
+    cursor.execute("SELECT * FROM user")
+    result = cursor.fetchall()
+    return result
