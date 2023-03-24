b44825acc210aba5eb9e1c16f869ba5b4c83c5ba
JanikCodes <piefke2000@gmx.de>
2023-03-25 00:38:31 +0100

    Adding first database logic

--- Commands/stats.py
+++ Commands/stats.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import database
-
 class stats(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -11,8 +10,13 @@ class stats(commands.Cog):
         print("Bot is Online. Beep bop.")
 
     @commands.command()
-    async def stats(selfself, ctx):
-        await ctx.send("Hey!")
+    async def stats(self, ctx):
+        print("Stats was called!")
+        if database.does_user_exist(ctx.author.id):
+            await ctx.send("You exist!")
+        else:
+            await ctx.send("You don't exist yet.. I'll create you in the database.")
+            database.add_user(ctx.author.id, ctx.author.name)
 
 async def setup(bot):
     await bot.add_cog(stats(bot))
--- database.py
+++ database.py
@@ -1,27 +1,38 @@
-import discord
-from discord.ext import commands
-import json
 import mysql.connector
 import TarnishedBot
 
 # Connect to the database
 mydb = mysql.connector.connect(
-    host= TarnishedBot.botConfig["host"],
-    user= TarnishedBot.botConfig["user"],
-    password= TarnishedBot.botConfig["password"],
-    port= TarnishedBot.botConfig["port"],
-    database= TarnishedBot.botConfig["database"],
+    host=TarnishedBot.botConfig["host"],
+    user=TarnishedBot.botConfig["user"],
+    password=TarnishedBot.botConfig["password"],
+    port=TarnishedBot.botConfig["port"],
+    database=TarnishedBot.botConfig["database"],
 )
 
 cursor = mydb.cursor()
 
 # Check if the connection is alive
 if mydb.is_connected():
-  print("Database connection successful")
+    print("Database connection successful")
 else:
-  print("Database connection failed")
+    print("Database connection failed")
 
-def add_user():
-    cursor.execute("SELECT * FROM user")
-    result = cursor.fetchall()
-    return result
+
+def add_user(idUser, userName):
+    sql = "INSERT INTO user VALUE(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, null, null, null, null, null)"
+    val = (idUser, userName, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1)
+    cursor.execute(sql, val)
+    print("Added new user with userName" + userName)
+
+
+def does_user_exist(idUser):
+    print("INIT")
+    sql = "SELECT * FROM user u WHERE u.idUser = %s"
+    val = (idUser,)
+    cursor.execute(sql, val)
+    res = cursor.fetchone()
+    if res:
+        return True
+    else:
+        return False
