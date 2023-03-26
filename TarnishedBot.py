6aa437e8426f0a0606537de82b4e665f913c32bd
cube_master <benjaminfinck02@googlemail.com>
2023-03-26 22:03:27 +0200

    cleaned up the code
    reformatted the print statements and strings
    cleaned up imports

--- TarnishedBot.py
+++ TarnishedBot.py
@@ -4,7 +4,6 @@ import discord
 from discord.ext import commands
 import config
 import db
-import logging
 from colorama import Back, Fore, Style
 
 intents = discord.Intents.all()
@@ -22,15 +21,13 @@ class Client(commands.Bot):
 
     async def on_ready(self):
         prfx = (Back.BLACK + Fore.GREEN + time.strftime("%H:%M:%S UTC", time.gmtime()) + Back.RESET + Fore.WHITE + Style.BRIGHT)
-        print(prfx + " Logged in as " + Fore.YELLOW + self.user.name)
-        print(prfx + " Bot ID " + Fore.YELLOW + str(self.user.id))
-        print(prfx + " Discord Version " + Fore.YELLOW + discord.__version__)
-        print(prfx + " Python Version " + Fore.YELLOW + str(platform.python_version()))
+        print(f"{prfx} Logged in as {Fore.YELLOW + self.user.name}")
+        print(f"{prfx} Bot ID {Fore.YELLOW + str(self.user.id)}")
+        print(f"{prfx} Discord Version {Fore.YELLOW + discord.__version__}")
+        print(f"{prfx} Python Version {Fore.YELLOW + str(platform.python_version())}")
         synced = await self.tree.sync()
-        print(prfx + " Slash CMDs Synced " + Fore.YELLOW + str(len(synced)) + " Commands")
+        print(f"{prfx} Slash CMDs Synced {Fore.YELLOW + str(len(synced))} Commands")
         await db.init_database()
-
-
 
 client = Client()
 client.run(config.botConfig["token"])
