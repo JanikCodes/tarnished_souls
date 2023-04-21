e2efd74ece49596406c29dadefc40d532fecc251
cube-m4st3r <benjaminfinck02@googlemail.com>
2023-04-21 18:16:41 +0200

    added insert_enemy button

--- Commands/developer.py
+++ Commands/developer.py
@@ -24,12 +24,31 @@ class CompleteQuestButton(discord.ui.Button):
 
         db.complete_quest(user=self.user)
 
+
+class InsertEnemyButton(discord.ui.Button):
+    def __init__(self, user):
+        super().__init__(label="Insert Enemy", style=discord.ButtonStyle.success)
+        self.user = user
+
+    async def callback(self, interaction: discord.Interaction):
+
+        if interaction.user.id != int(self.user.get_userId()):
+            embed = discord.Embed(title=f"You're not allowed to use this action!",
+                                  description="",
+                                  colour=discord.Color.red())
+            return await interaction.response.send_message(embed=embed, ephemeral=True, delete_after=2)
+
+        await interaction.response.defer()
+
+
 class DeveloperView(discord.ui.View):
 
     def __init__(self, user):
         super().__init__()
         self.user = user.update_user()
         self.add_item(CompleteQuestButton(user=user))
+        self.add_item(InsertEnemyButton(user=user))
+
 
 class Developer(commands.Cog):
     def __init__(self, client: commands.Bot):
@@ -41,7 +60,7 @@ class Developer(commands.Cog):
             if db.validate_user(interaction.user.id):
                 user = User(interaction.user.id)
 
-                if interaction.user.id == 321649314382348288:
+                if interaction.user.id == 321649314382348288 or interaction.user.id == 348781333839085569:
 
                     embed = discord.Embed(title=f"Developer options",
                                           description="What do you want to do next?")
@@ -53,6 +72,7 @@ class Developer(commands.Cog):
                 await class_selection(interaction=interaction)
         except Exception as e:
             await self.client.send_error_message(e)
+
 
 async def setup(client: commands.Bot) -> None:
     await client.add_cog(Developer(client))
